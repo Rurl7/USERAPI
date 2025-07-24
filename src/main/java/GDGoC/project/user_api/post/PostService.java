f81425a6780dcd4fcf588d9315abc6249f5f1d91
acleruiys <acleruiys@gmail.com>
2025-07-24 10:44:04 +0900

    [Feat] add getPost, getList in PostService

--- src/main/java/GDGoC/project/user_api/post/PostService.java
+++ src/main/java/GDGoC/project/user_api/post/PostService.java
@@ -1,9 +1,27 @@
 package GDGoC.project.user_api.post;
 
+import GDGoC.project.user_api.exception.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
 public class PostService {
+    private final PostRepository postRepository;
+
+    public List<Post> getList(){
+        return postRepository.findAll();
+    }
+
+    public Post getPost(Long id){
+        Optional<Post> question = this.postRepository.findById(id);
+        if(question.isPresent()){
+            return question.get();
+        } else {
+            throw new DataNotFoundException("post not found");
+        }
+    }
 }
