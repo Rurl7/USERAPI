a0e11424abbc65ac112b254ddab5f5fc5633e61d
acleruiys <acleruiys@gmail.com>
2025-07-29 13:14:20 +0900

    [Chore] delete like

--- src/main/java/GDGoC/project/user_api/post/LikeRepository.java
+++
@@ -1,8 +0,0 @@
-package GDGoC.project.user_api.post;
-
-import GDGoC.project.user_api.member.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LikeRepository extends JpaRepository<Like, Long> {
-    boolean existsByPostAndMember(Post post, Member member);
-}
