f445603521462a6b0656337fece8e43ce8c8dea9
acleruiys <acleruiys@gmail.com>
2025-07-30 13:30:33 +0900

    [Chore] add ResponseBody

--- src/main/java/GDGoC/project/user_api/MainController.java
+++ src/main/java/GDGoC/project/user_api/MainController.java
@@ -2,12 +2,13 @@ package GDGoC.project.user_api;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
     @GetMapping("/")
+    @ResponseBody
     public String root() {
-        /* localhost:8080로 접속하면 localhost:8080/signin로 주소가 바뀌면서 질문 목록이 있는 웹 페이지로 연결된다. */
-        return "redirect:/signin";
+        return "home";
     }
 }
